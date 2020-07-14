420cb4616f590b2abd49f48d89852069ce4e6de8
pratap_narayan <pratap_narayan@apple.com>
2020-07-14 14:12:46 +0530

    stub introduced

--- mockito-testing/src/main/java/com/pratap/unittesting/business/SomeBusinessImpl.java
+++ mockito-testing/src/main/java/com/pratap/unittesting/business/SomeBusinessImpl.java
@@ -1,9 +1,28 @@
 package com.pratap.unittesting.business;
 
+import com.pratap.unittesting.data.SomeDataService;
+
 public class SomeBusinessImpl {
+	
+	// dataService talking to database
+	private SomeDataService dataService;
+
+	public void setDataService(SomeDataService dataService) {
+		this.dataService = dataService;
+	}
 
 	public int calculateSum(int[] data) {
 		int sum = 0;
+		for(int value : data) {
+			sum +=value;
+		}
+		
+		return sum;
+	}
+	
+	public int calculateSumUsingDataService() {
+		int sum = 0;
+		int[] data = dataService.retrieveAllData();
 		for(int value : data) {
 			sum +=value;
 		}
--- mockito-testing/src/test/java/com/pratap/unittesting/business/SomeBusinessImplTest.java
+++ mockito-testing/src/test/java/com/pratap/unittesting/business/SomeBusinessImplTest.java
@@ -4,6 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.pratap.unittesting.data.SomeDataService;
+
+class SomeDataServiceStub implements SomeDataService{
+
+	@Override
+	public int[] retrieveAllData() {
+		return new int[] {1, 2, 3};
+	}
+}
+
+
 class SomeBusinessImplTest {
 
 	@Test
@@ -27,6 +38,15 @@ class SomeBusinessImplTest {
 		SomeBusinessImpl business = new SomeBusinessImpl();
 		int actualResult = business.calculateSum(new int[] {5});
 		int expectedResult = 5;
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	void testCalculateSumUsingDataServiceBasic() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		business.setDataService(new SomeDataServiceStub());
+		int actualResult = business.calculateSumUsingDataService();
+		int expectedResult = 6;
 		assertEquals(expectedResult, actualResult);
 	}
 
