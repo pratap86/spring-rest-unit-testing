0aa592dce6c66aac08fd15c2d7c5092f39b3ffe0
pratap_narayan <pratap_narayan@apple.com>
2020-07-15 11:43:17 +0530

    initial rest controller unit test

---
+++ mockito-testing/src/main/java/com/pratap/unittesting/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.pratap.unittesting.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+}
