9b8ed9885536951e2220046f7f3d077372aeb284
pratap_narayan <pratap_narayan@apple.com>
2020-07-15 11:54:17 +0530

    verify status and content

--- mockito-testing/src/test/java/com/pratap/unittesting/controllers/HelloWorldControllerTest.java
+++ mockito-testing/src/test/java/com/pratap/unittesting/controllers/HelloWorldControllerTest.java
@@ -1,13 +1,13 @@
 package com.pratap.unittesting.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -22,10 +22,11 @@ class HelloWorldControllerTest {
 
 		// call helloWorld
 		RequestBuilder request = MockMvcRequestBuilders.get("/hello-world").accept(MediaType.APPLICATION_JSON);
-		MvcResult result = mockMvc.perform(request).andReturn();
+		mockMvc.perform(request)
+							.andExpect(status().isOk())
+							.andExpect(content().string("Hello World"))
+							.andReturn();
 		
-		// verify
-		assertEquals("Hello World", result.getResponse().getContentAsString());
 	}
 
 }
